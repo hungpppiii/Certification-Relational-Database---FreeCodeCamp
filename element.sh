6fc5d5c1eaa134795bf34c96e43a0bf1bb476c68
The Hung <hungppp303@gmail.com>
2024-08-06 15:42:05 +0000

    fix: fix logic

--- element.sh
+++ element.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 PSQL="psql --username=postgres --dbname=periodic_table -t --no-align -c"
 
-echo "Please provide an element as an argument."
-INPUT=$1
-
-IFS="|" read ATOMIC_NUMBER SYMBOL ELEMENT_NAME <<< $($PSQL "SELECT * FROM elements WHERE atomic_number = '$INPUT' OR symbol = '$INPUT' OR name = '$INPUT'")
-
-if [[ -n $ATOMIC_NUMBER ]]
+if [[ -z $1 ]]
 then
-  echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  echo "Please provide an element as an argument."
 else
-  echo "I could not find that element in the database."
+  IFS="|" read ATOMIC_NUMBER SYMBOL ELEMENT_NAME <<< $($PSQL "SELECT * FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1'")
+
+  if [[ -n $ATOMIC_NUMBER ]]
+  then
+    echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  else
+    echo "I could not find that element in the database."
+  fi
 fi
