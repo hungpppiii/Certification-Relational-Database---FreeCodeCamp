b95003198d26219a2cea13c95b97d5f164d3ff0d
The Hung <hungppp303@gmail.com>
2024-08-06 16:04:37 +0000

    feat: complete

--- element.sh
+++ element.sh
@@ -5,11 +5,17 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  IFS="|" read ATOMIC_NUMBER SYMBOL ELEMENT_NAME <<< $($PSQL "SELECT * FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1'")
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    IFS="|" read ATOMIC_NUMBER SYMBOL ELEMENT_NAME <<< $($PSQL "SELECT * FROM elements WHERE atomic_number = $1")
+  else
+    IFS="|" read ATOMIC_NUMBER SYMBOL ELEMENT_NAME <<< $($PSQL "SELECT * FROM elements WHERE symbol = '$1' OR name = '$1'")
+  fi
 
   if [[ -n $ATOMIC_NUMBER ]]
   then
-    echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+    IFS="|" read TYPE MASS MELTING_POINT BOILING_POINT <<< $($PSQL "SELECT t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM properties p JOIN types t ON p.type_id = t.type_id WHERE p.atomic_number = '$ATOMIC_NUMBER'")
+    echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   else
     echo "I could not find that element in the database."
   fi
